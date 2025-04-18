3a749dc360a331f36df7c6a487e079a05a514df3
Hobart2967 <hobart2967@outlook.com>
2025-04-18 02:49:24 +0200

    fix(docker): :bug: Fix push to docker

--- release.sh
+++ release.sh
@@ -1,2 +1,2 @@
-docker push Hobart2967/docker-spigot-mc-$MC_VERSION:$1
-docker push Hobart2967/docker-spigot-mc-$MC_VERSION:latest
+docker push docker-spigot-mc-$MC_VERSION:$1
+docker push docker-spigot-mc-$MC_VERSION:latest
