25c7feeada7afd755889f3c68998eca3153fdf6e
Hobart2967 <hobart2967@outlook.com>
2025-04-18 12:12:53 +0200

    ci(core): :bug: Fix release names as well

--- .releaserc.js
+++ .releaserc.js
@@ -1,23 +1,25 @@
 module.exports = {
   branches: ['main'],
   tagFormat: `${process.env.MC_VERION}-v${version}`,
-	plugins: [
-		[
-			'@semantic-release/commit-analyzer',
-			{
-				preset: 'angular',
-				releaseRules: [
-					{ type: 'docs', scope: 'README', release: 'patch' },
-					{ type: 'refactor', release: 'patch' },
-					{ type: 'style', release: 'patch' },
-				],
-				parserOpts: {
-					noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES'],
-				},
-			},
-		],
-		'@semantic-release/release-notes-generator',
-		'@semantic-release/github',
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'angular',
+        releaseRules: [
+          { type: 'docs', scope: 'README', release: 'patch' },
+          { type: 'refactor', release: 'patch' },
+          { type: 'style', release: 'patch' },
+        ],
+        parserOpts: {
+          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES'],
+        },
+      },
+    ],
+    '@semantic-release/release-notes-generator',
+    ['@semantic-release/github', {
+      releaseNameTemplate: process.env.MC_VERION + '-v<%= nextRelease.version %>',
+    }],
 		[
 			'@semantic-release/exec',
 			{
